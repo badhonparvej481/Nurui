1bb5549d357094e36e77d344bf3be0cc258cd1da
afsar mahmud <mdafsar.dev@gmail.com>
2025-03-09 15:04:00 +0600

    contact-us page responsive done

--- src/components/main/contact-us/ContactUsSection.tsx
+++ src/components/main/contact-us/ContactUsSection.tsx
@@ -6,8 +6,8 @@ import ShinyTextArea from "@/components/common/ShinyTextArea";
 import ButtonBackgroundShineButton from "@/components/common/ButtonBackgroundShine";
 const ContactUsSection = () => {
   return (
-    <div className="container pt-10 flex gap-24">
-      <div className="w-1/3 bg-gradient-to-t from-[var(--glass-color)] to-[var(--primary-color-2)] rounded-2xl p-14 space-y-8">
+    <div className="container pt-6 lg:pt-10 flex flex-col lg:flex-row gap-8 xl:gap-14 2xl:gap-24">
+      <div className="lg:w-1/3 bg-gradient-to-t from-[var(--glass-color)] via-[var(--glass-color)] to-[var(--primary-color-2)] rounded-2xl p-6 2xl:p-14 space-y-8">
         <h3 className="text-2xl font-black text-[var(--primary-color)]">
           Get in touch
         </h3>
@@ -33,7 +33,7 @@ const ContactUsSection = () => {
           </div>
         </div>
       </div>
-      <form className="w-2/3 grid grid-cols-2 gap-6">
+      <form className="lg:w-2/3 grid lg:grid-cols-2 gap-6">
         <fieldset className="space-y-2">
           <label className="capitalize font-semibold">First name</label>
           <ShinyInput
